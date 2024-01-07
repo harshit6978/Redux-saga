d16eeba6daa6035278097678d1a65beecc18edee
harshit <126167058+harshit6978@users.noreply.github.com>
2024-01-07 22:38:35 +0530

    first commit

---
+++ src/Redux-saga/rootReducer.js
@@ -0,0 +1,10 @@
+import { combineReducers } from "redux";
+// import productReducer from "./admin/reducer/reducer";
+import productReducer from "./Admin/Reducer/reducer";
+
+const rootReducers = combineReducers({
+    productReducer,
+
+});
+
+export default rootReducers;
